accd7575a975cd3b586a12e80577ffe5782d730a
Shreyas2007 <84057370+shreyas391@users.noreply.github.com>
2023-05-13 19:26:57 +0530

    if else statement updated

---
+++ main.py
@@ -0,0 +1,13 @@
+my_mistake = "small"
+
+if my_mistake == "small" :
+    return scold()
+
+elif my_mistake == "medium" :
+    return situp()
+
+elif my_mistake == "big" :
+    return "beat"
+
+else forgiven = true :
+    
